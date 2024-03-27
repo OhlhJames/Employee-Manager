4e681fdcbb69f84d44c56207e274be117e459c42
IsItIzzet <ohlhjames@gmail.com>
2024-03-27 18:33:52 -0500

    added starting logic to the index file so that users can begin to interact with the database

--- index.js
+++ index.js
@@ -18,3 +18,52 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+inquirer
+  .prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name:'intro',
+        choices:['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    }
+  ])
+  .then((data) => {
+    const introChoice = data.intro
+    switch(introChoice){
+        case 'View All Employees':
+            db.query('SELECT * FROM employee', function(err, res) {
+                console.log(results)
+            });
+            break;
+        case 'Add Employee':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'What is the employees  first name?',
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message:'What is the employees last name?',
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is the employees role?',
+                    choices: [db.query('SELECT name FROM department')]
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: 'Who is the employees manager?',
+                    choices: ['None', db.query('SELECT name FROM employee')]
+                },
+                {
+                    
+                }
+            ])
+            
+    }
+  });
