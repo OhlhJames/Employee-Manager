5f1dfd3f1e63474f23c1c171a56a583a70bf77c2
IsItIzzet <ohlhjames@gmail.com>
2024-03-27 16:57:28 -0500

    Added starting server connection code as well as index.js for the inquirer package to run from

---
+++ index.js
@@ -0,0 +1,20 @@
+// These lines are so that the javascript can access the Filesystem and Inquirer packages
+const inquirer = require('inquirer');
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
