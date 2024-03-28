e1301fe4406636e9f28839bcbd2b0ae503937dfe
IsItIzzet <ohlhjames@gmail.com>
2024-03-28 15:01:07 -0500

    added coding into the roles area so users can update employee roles

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+
 inquirer.prompt([
     {
         type: 'list',
@@ -29,7 +30,19 @@ inquirer.prompt([
 ])
 .then((data) => {
     const introChoice = data.intro
-    switch(introChoice){
+    db.query('SELECT name FROM department', function(err,res) {
+        const deptList = results;
+        return deptList;
+    });
+    db.query('SELECT name FROM employee', function(err,res) {
+        const employeeList = results;
+        return employeeList;
+    });
+    db.query('SELECT title FROM role', function(err,res) {
+        const roleList = results;
+        return roleList;
+    });
+    switch(introChoice, employeeList, deptList){
         case 'View All Employees':
             db.query('SELECT * FROM employee', function(err, res) {
                 console.log(results)
@@ -51,13 +64,13 @@ inquirer.prompt([
                     type: 'list',
                     name: 'role',
                     message: 'What is the employees role?',
-                    choices: [db.query('SELECT name FROM department')]
+                    choices: [deptList]
                 },
                 {
                     type: 'list',
                     name: 'manager',
                     message: 'Who is the employees manager?',
-                    choices: ['None', db.query('SELECT name FROM employee')]
+                    choices: ['None', employeeList]
                 }
             ])
             .then((data) => {
@@ -65,6 +78,23 @@ inquirer.prompt([
             });
             break;
         case 'Update Employee Role':
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employeeSelect',
+                    message: 'Which employee would you like to update?',
+                    choices:[employeeList]
+                },
+                {
+                    type: 'list',
+                    name: 'roleSelect',
+                    message: 'Which role should they receive?',
+                    choices:[roleList]
+                }
+            ])
+            .then((data) =>{
+                db.query(`UPDATE employee SET role_id = "${data.roleSelect}" WHERE id = ${employeeSelect}`)
+            });
             break;
         case 'View All Roles':
             db.query(`SELECT * FROM department;`, function (err, results) {
@@ -72,6 +102,30 @@ inquirer.prompt([
             });
             break;
         case 'Add Role':
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name:'roleTitle',
+                    message: 'What is the title of this role?',
+                },
+                {
+                    type: 'input',
+                    name: 'rolePay',
+                    message: 'What is the salary for this position?',
+                },
+                {
+                    type: 'list',
+                    name:'roleDept',
+                    message:'Which department does this role belong to?',
+                    choices:[deptList]
+                }
+            ])
+            .then((data) =>{
+                db.query(`INSERT INTO department (title, salary, department_id) VALUES ("${data.roleTitle}","${data.rolePay}","${data.roleDept}");`);
+                db.query(`SELECT * FROM role;`, function (err, results) {
+                    console.log(results)
+                });
+            });
             break;
         case 'View All Departments':
             db.query(`SELECT * FROM department;`, function (err, results) {
